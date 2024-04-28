2d44989560b97674fa074236a116be56def92bea
Oksana Shkarbun <Oksana.Shkarbun@withsecure.com>
2024-04-28 21:03:02 +0300

    fix naming

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title 1', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('get started link 2', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
